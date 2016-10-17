0b1662fbbff141783f677863c3f330554a0ac3ce
Changkun Ou <ouchangkun@outlook.com>
2016-10-17 10:00:18 +0200

    Add a glance code.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# munich-hoursing-tool
-A hoursing location visualization tool for munich.
+# Renting Tool for Munich
+
+A housing location visualization tool for Munich.
+
+> This repository is still in developing, please feel free to submit an [issue](https://github.com/changkun/munich-housing-tool/issues) or pull a [request](https://github.com/changkun/munich-housing-tool/pulls) if you have any new idea or requirements.
+
+## Motivation
+
+It's generally known that renting in Munich is a huge probelm for everyone. As a fresh student, we can not afford such high rents over than 600 Euro if we do not applied a student domitory.
+
+The **Studentenwerk Muenchen** provides Housing Referral Service, which has a loads of housing informations. However, unfortunately, their information lists in a table and quite difficult to use for us. 
+
+Thus, a convenient renting tool becomes a necessity.
+
+## Demo
+
+![](imgs/overview.png)
+
+## Usage
+
+### `housing-spider.py`
+
+`housing-spider.py` is a simple spider to download housing infomations from **Studentenwerk Muenchen**, just simply run:
+
+```bash
+python housing-spider.py
+```
+
+Then you should get a `.csv` file in this folder, which contains all the housing informations provided by Studentenwerk Muenchen.
+
+### `index.html`
+
+Once you have the `rent.csv` file, you should be able to use `index.html` to visualize all the informations in a map that allows you to check all the housing infos quickly and intuitional.
+
+For usage, just simply choose the `.csv` file in the web page, then you will get all you need.
+
+
+## License
+
+This repository is under the [MIT license](LICENSE), you must read license details before you use it.
---
+++ housing-spider.py
@@ -0,0 +1,125 @@
+# -- encoding: utf-8 --
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+# studentenwerk rent url
+base_url = "http://www.studentenwerk-muenchen.de"
+offers_uri = "/wohnen/privatzimmervermittlung/angebote/"
+
+illustrate_de = [
+    'Stadtteil',
+    'Straße',
+    'Art des Zimmers',
+    'Möblierung',
+    'Größe',
+    'Anzahl Zimmer',
+    'Kaltmiete',
+    'Nebenkosten',
+    'Gesamtmiete',
+    'Kaution',
+    'möglich ab',
+    'Befristung',
+    'Nebenleistung',
+    'Weitere Nebenleistungen',
+    'Besonderheiten',
+    'Verkehrsmittel',
+    'Entfernung',
+    'Bemerkung',
+    'url'
+]
+illustrate_en = [
+    'District',
+    'Road',
+    'Type of room',
+    'Furnishing',
+    'Size',
+    'Number of Rooms',
+    'Cold rent',
+    'Additional costs',
+    'Total rent',
+    'Deposit',
+    'Possible ab',
+    'Time limit',
+    'Ancillary service',
+    'Other ancillary services',
+    'Particularities',
+    'Transport',
+    'Distance',
+    'Remark'
+    'Url'
+]
+illustrate_cn = [
+    '区',
+    '街道',
+    '房屋类型',
+    '家具',
+    '房屋大小',
+    '房屋数量',
+    '基本租金',
+    '费用',
+    '总租金',
+    '押金',
+    '开始时间',
+    '结束时间',
+    '辅助设施',
+    '其他设施',
+    '特殊设施',
+    '交通',
+    '距离',
+    '备注',
+    'URL'
+]
+
+print "fetch infomations..."
+response = requests.get(base_url+offers_uri)
+html = BeautifulSoup(response.text, "html.parser")
+
+# data container
+offers = {
+    'muenchen': [],
+    'freising': [],
+    'rosenheim':[]
+}
+
+# parse infos
+tables = html.find_all('table', attrs = {
+    'class': 'tx_stwmprivatzimmervermittlung'
+})
+
+# fetch muenchen offers
+table_body = tables[0].find('tbody')
+rows = table_body.find_all('tr')
+
+for i, row in enumerate(rows):
+
+    offer = [col.text.strip() for col in row.find_all('td')]
+    offer = [col.encode('utf-8') for col in offer if col]
+
+    uris = [uri['href'] for uri in row.find_all('a')]
+    uris  = [col for col in uris if col]
+    offer_response = requests.get(base_url+uris[0])
+    offer_details = BeautifulSoup(offer_response.text, "html.parser")
+    uls = offer_details.find('ul', attrs = {
+        'class': '[ o-list-ui o-list-ui--flush ]'
+    })
+
+    infos = []
+    for li in uls.find_all('li', {'class': 'o-media'}):
+        info = li.find('span', {'class': 'o-media__body'}).string
+        if info is not None:
+            info = unicode(info)
+        else:
+            info = 'Nothing'
+        info = info.replace('\n', ' ').replace('\t', '').replace('  ', '').strip(' ')
+        infos.append(info.encode('utf-8'))
+
+    # print 'offer:', offer
+    print 'infos:', infos
+
+    offers['muenchen'].append(infos+uris)
+
+with open("rent.csv", "wb") as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',')
+    csv_writer.writerow(illustrate_cn)
+    csv_writer.writerows(offers['muenchen'])
